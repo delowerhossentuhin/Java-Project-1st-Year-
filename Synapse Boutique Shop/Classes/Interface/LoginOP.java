c57b1caa7df667c08ad81016996beb11a42a639c
D.H. Tuhin <128525189+delowerhossentuhin@users.noreply.github.com>
2024-11-01 16:56:51 +0600

    Delete Synapse Boutique Shop directory

--- Synapse Boutique Shop/Classes/Interface/LoginOP.java
+++
@@ -1,9 +0,0 @@
-package Interface;
-//import Classes.*;
-import java.lang.*;
-
-public interface LoginOP {
-    void matchAdmin(String s1, String s2);
-
-    void matchUser(String s1, String s2);
-}
